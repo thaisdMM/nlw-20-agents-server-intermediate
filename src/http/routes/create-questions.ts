467dda6696fc6b8bfab6df09887e096f5534c28e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-07-11 14:42:57 +0200

    feat: implement final Gemini answer flow and question persistence
    - Added prompt context and instructions to generateAnswer function
    - Connected vector search results to answer generation
    - Stored questions in DB using Drizzle ORM

--- src/http/routes/create-questions.ts
+++ src/http/routes/create-questions.ts
@@ -1,8 +1,11 @@
 // rota para criar salas
+
+import { and, eq, sql } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { generateAnswer, generateEmbeddings } from '../../services/gimini.ts'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -21,15 +24,47 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { roomId } = request.params
       const { question } = request.body
 
+      const embeddings = await generateEmbeddings(question)
+
+      const embeddingsAsString = `[${embeddings.join(',')}]`
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings}<=>${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings}<=>${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings}<=>${embeddingsAsString}::vector`
+        )
+        .limit(3)
+
+      let answer: string | null = null
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+        answer = await generateAnswer(question, transcriptions)
+      }
+
       const result = await db
         .insert(schema.questions)
-        .values({ roomId, question })
+        .values({ roomId, question, answer })
         .returning()
+
       const insertedQuestion = result[0]
+
       if (!insertedQuestion) {
         throw new Error('Failed to create new room.')
       }
-      return reply.status(201).send({ questionId: insertedQuestion.id })
+      return reply.status(201).send({ questionId: insertedQuestion.id, answer })
     }
   )
 }
